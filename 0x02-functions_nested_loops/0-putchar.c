606ecf3b0cacd8fe35c6458c11e1b5601fedb18f
IdyOJ60 <chudioji@gmail.com>
2023-03-24 12:59:38 -0700

    isupper

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+
+
+/**
+ * MAIN -Entry point of my code,
+ *
+ * Reurn: Always 0
+ */
+
+int main(void)
+{
+_putchar('_');
+_putchar('P');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
+return (0);
+}
